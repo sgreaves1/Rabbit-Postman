877df8d17d79054aaffdbd41bbdeb380a65c47bb
Sam Greaves <samgreaves0@gmail.com>
2018-11-01 09:02:39 +0000

    RP-1 #comment add html css and js files

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ const electron = require('electron');
 const app = electron.app;
 const browserWindow = electron.BrowserWindow;
 
+let mainWindow;
+
 app.on('ready', _ => {
-    new browserWindow({
+    mainWindow = new browserWindow({
         width:400,
         height:400
     });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    mainWindow.on('closed', _ => {
+        mainWindow = null;
+    });
 })
+
