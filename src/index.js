6a7621fead416d608c16e96d9c0a3b02bab27530
Sam Greaves <samgreaves0@gmail.com>
2018-11-02 09:53:52 +0000

    RP-1 #comment add payload id to html text area

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 const rabbitHelper = require('./rabbitmq');
 
-console.log('In renderer!');
-
  function submit() {
     var rabbitUrl, payload, exchange, routingeKey;
     //
     // rabbitUrl = document.getElementById("rabbituri").value;
-    // payload = document.getElementById("payload").value;
+     payload = document.getElementById("payload").value;
     // routingeKey = document.getElementById("routingKey").value;
     // exchange = document.getElementById("exchange").value;
     //
-    // rabbitUrl = 'amqp://guest:fishcake@local:5672/quoting#quoting'
-    // exchange = 'quoting:DeleteAccountHandler';
-    // routingKey = 'quoting:DeleteAccountHandler';
+    rabbitUrl = 'amqp://guest:fishcake@local:5672/quoting'
+    exchange = 'quoting:DeleteAccountHandler';
+    routingKey = 'quoting:DeleteAccountHandler';
 
     console.log('here');
     rabbitHelper.insertMessage(payload, rabbitUrl, exchange, routingKey);
