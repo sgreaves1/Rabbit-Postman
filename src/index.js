9fc4e44f94cac8f818f03428f7e862906b9ec721
Sam Greaves <samgreaves0@gmail.com>
2018-11-02 16:47:51 +0000

    RP-6 #comment pushed url through from ui

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ const rabbitHelper = require('./rabbitmq');
  function submit() {
     var rabbitUrl, payload, queue, deadLetterExchange;
 
-    // rabbitUrl = document.getElementById("rabbituri").value;
+    rabbitUrl = document.getElementById("rabbituri").value;
     payload = document.getElementById("payload").value;
     // deadLetterExchange = document.getElementById("deadLetterExchange").value;
      queue = document.getElementById('queue').value;
 
-    rabbitUrl = 'amqp://guest:fishcake@localhost:5672/quoting'
-    //queue = 'quoting:DeleteAuxiliaryDataByReconIdHandler';
     deadLetterExchange = 'quoting:dead.letter.exchange';
     
     rabbitHelper.insertMessage(payload, rabbitUrl, queue, deadLetterExchange);
