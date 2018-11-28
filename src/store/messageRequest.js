31d37c73d260a860ca8bd2e73ee1fe95fb763366
Sam Greaves <samgreaves0@gmail.com>
2018-11-28 09:47:21 +0000

    RP-29 missed this file

--- src/store/messageRequest.js
+++ src/store/messageRequest.js
@@ -1,11 +1,11 @@
-module.exports = class messageRequest {
-    constructor(name, url, description, deadLetterExchange, queue, payload) {
-        this.name = name;
-        this.url = url;
-        this.description = description;
-        this.deadLetterExchange = deadLetterExchange;
-        this.queue = queue;
-        this.payload = payload;
-
-    }
-}
+// module.exports = messageRequest() {
+//     constructor(name, url, description, deadLetterExchange, queue, payload) {
+//         this.name = name;
+//         this.url = url;
+//         this.description = description;
+//         this.deadLetterExchange = deadLetterExchange;
+//         this.queue = queue;
+//         this.payload = payload;
+//
+//     }
+// }
