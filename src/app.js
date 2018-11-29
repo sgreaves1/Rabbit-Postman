880681b57b1ebfc43b07d78121e52bd4d790ea3d
Sam Greaves <samgreaves0@gmail.com>
2018-11-29 10:29:09 +0000

    RP-29 Move onclick code to select panel component

--- src/app.js
+++ src/app.js
@@ -3,106 +3,16 @@ import './app.css';
 import SelectPanel from './components/SelectPanel'
 import ToolBar from './components/ToolBar'
 import MainBody from './components/MainBody'
-
-
-// const rabbitHelper = require('./rabbitmq');
 import UserData from './store/UserData';
-
-// const messageRequest = require('./store/messageRequest');
-// const {remote} = require('electron');
-// //const {ipcRenderer} = window.require('electron');
-// const {Menu, MenuItem} = remote;
-//
-// let selectedItem = null;
-//
-// const menu = new Menu();
-// menu.append(new MenuItem({label: 'Delete', click() {
-//     db.deleteRequest(selectedItem.name);
-// }}));
-//
-//
-//
-// function updateView(item) {
-//     document.getElementById("rabbituri").value = item.url;
-//     document.getElementById("payload").value = item.payload;
-//     document.getElementById("deadLetterExchange").value = item.deadLetterExchange;
-//     document.getElementById('queue').value = item.queue;
-// }
-//
-
-
-
-//
-//  function send() {
-//     var rabbitUrl, payload, queue, deadLetterExchange;
-//
-//     rabbitUrl = document.getElementById("rabbituri").value;
-//     payload = document.getElementById("payload").value;
-//     deadLetterExchange = document.getElementById("deadLetterExchange").value;
-//     queue = document.getElementById('queue').value;
-//
-//     rabbitHelper.insertMessage(payload, rabbitUrl, queue, deadLetterExchange);
-// }
-//
-// function save() {
-//
-//     try {
-//         // ipcRenderer.on('save-reply', (event, arg1, arg2) => {
-//         //     let request = new messageRequest();
-//         //     request.name = arg1;
-//         //     request.description = arg2;
-//         //     request.url = document.getElementById("rabbituri").value;
-//         //     request.deadLetterExchange = document.getElementById("deadLetterExchange").value;
-//         //     request.queue = document.getElementById('queue').value;
-//         //     request.payload = document.getElementById("payload").value;
-//         //
-//         //     let user = db.loadUser();
-//         //     user.requests.push(request);
-//         //
-//         //     db.storeUser(user);
-//         //
-//         //     console.log('Saved');
-//         // });
-//         //
-//         // ipcRenderer.send('show-saveRequest');
-//
-//
-//
-//     } catch (error)
-//     {
-//         console.log(error);
-//     }
-// }
-//
-// let open = false;
-// function saveAsDropdownButtonClick() {
-//     open = !open;
-//
-//     document.getElementById("saveAsDropdownList").hidden = !open;
-// }
-//
-// window.send = send;
-// window.save = save;
-// window.saveAsDropdownButtonClick = saveAsDropdownButtonClick;
 
 class App extends Component {
 
     load() {
         this.userData = new UserData();
         this.userData.requests = [{name:'Sam'}, {name:"Tim"}];
-        // this.user =  new Store().get('userData', new userData());
-
-//         button.addEventListener('contextmenu', (e) => {
-//             e.preventDefault();
-//             selectedItem = item;
-//             menu.popup({window: remote.getCurrentWindow()});
-//         }, false);
-
-//         button.onclick = function() {updateView(item);};
     }
 
     render() {
-
         this.load();
 
         return (
--- src/components/SelectPanel.js
+++ src/components/SelectPanel.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 class SelectPanel extends React.Component {
+    selectedItemOnClick(selectedItem) {
+        //     document.getElementById("rabbituri").value = item.url;
+//     document.getElementById("payload").value = item.payload;
+//     document.getElementById("deadLetterExchange").value = item.deadLetterExchange;
+//     document.getElementById('queue').value = item.queue;
+    }
+
     render()
     {
-        const buttons = this.props.selectableItems.map((items) =>
-            <button>{items.name}</button>
+        const buttons = this.props.selectableItems.map((item) =>
+            <button onClick={this.selectedItemOnClick(item)}>{item.name}</button>
         );
 
         return <div className="selectPanel" id="selectPanel">
@@ -14,3 +21,88 @@ class SelectPanel extends React.Component {
 }
 
 export default SelectPanel;
+
+
+// this.user =  new Store().get('userData', new userData());
+
+
+//         button.addEventListener('contextmenu', (e) => {
+//             e.preventDefault();
+//             selectedItem = item;
+//             menu.popup({window: remote.getCurrentWindow()});
+//         }, false);
+
+
+
+
+
+
+
+// const rabbitHelper = require('./rabbitmq');
+// const messageRequest = require('./store/messageRequest');
+// const {remote} = require('electron');
+// //const {ipcRenderer} = window.require('electron');
+// const {Menu, MenuItem} = remote;
+//
+// let selectedItem = null;
+//
+// const menu = new Menu();
+// menu.append(new MenuItem({label: 'Delete', click() {
+//     db.deleteRequest(selectedItem.name);
+// }}));
+//
+
+
+
+//
+//  function send() {
+//     var rabbitUrl, payload, queue, deadLetterExchange;
+//
+//     rabbitUrl = document.getElementById("rabbituri").value;
+//     payload = document.getElementById("payload").value;
+//     deadLetterExchange = document.getElementById("deadLetterExchange").value;
+//     queue = document.getElementById('queue').value;
+//
+//     rabbitHelper.insertMessage(payload, rabbitUrl, queue, deadLetterExchange);
+// }
+//
+// function save() {
+//
+//     try {
+//         // ipcRenderer.on('save-reply', (event, arg1, arg2) => {
+//         //     let request = new messageRequest();
+//         //     request.name = arg1;
+//         //     request.description = arg2;
+//         //     request.url = document.getElementById("rabbituri").value;
+//         //     request.deadLetterExchange = document.getElementById("deadLetterExchange").value;
+//         //     request.queue = document.getElementById('queue').value;
+//         //     request.payload = document.getElementById("payload").value;
+//         //
+//         //     let user = db.loadUser();
+//         //     user.requests.push(request);
+//         //
+//         //     db.storeUser(user);
+//         //
+//         //     console.log('Saved');
+//         // });
+//         //
+//         // ipcRenderer.send('show-saveRequest');
+//
+//
+//
+//     } catch (error)
+//     {
+//         console.log(error);
+//     }
+// }
+//
+// let open = false;
+// function saveAsDropdownButtonClick() {
+//     open = !open;
+//
+//     document.getElementById("saveAsDropdownList").hidden = !open;
+// }
+//
+// window.send = send;
+// window.save = save;
+// window.saveAsDropdownButtonClick = saveAsDropdownButtonClick;
